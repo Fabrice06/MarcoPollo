0b05429348fabbf0ee7119d75d434f11f72d07d5
Fabrice <Fabrice@MacBook-Pro-de-Fabrice.local>
2016-01-11 22:11:36 +0100

    Appel Rest sur le front

--- static/js/app.js
+++ static/js/app.js
@@ -17,16 +17,25 @@ myApp.config(['$routeProvider', function ($routeProvider) {
       });
   }]);
 
-myApp.controller('personCtrl', function ($scope) {
-	  $scope.listePersonnes = [
-	    {lastName: 'TOTO',firstName: 'Paul'},
-	    {lastName: 'LULU',firstName: 'Isa'},
-	    {lastName: 'BRIBRI',firstName: 'Carole'}
-	  ];
-	  $scope.unePersonne = {lastName:'Kiki'};
+myApp.controller('personCtrl', function ($scope, $http) {
+	$http.get('http://localhost:8080/persons').
+		success(function(data){
+			$scope.listePersonnes = data;
+			$scope.unePersonne = data[0];
+		});	
 	});
+	
 })();
 
 
 
 
+
+/*
+ $scope.listePersonnes = [
+	    {lastName: 'TOTO',firstName: 'Paul'},
+	    {lastName: 'LULU',firstName: 'Isa'},
+	    {lastName: 'BRIBRI',firstName: 'Carole'}
+	  ];
+	  $scope.unePersonne = {lastName:'Kiki'};
+	  */
