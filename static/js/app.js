38d20d65c78c9e8f7dcc829b1461902c6f248376
Cecile <cecile.cazaux@u-psud.fr>
2016-01-14 18:19:57 +0100

    split 3 modules (controller/service/app)

--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,21 @@
 (function () {
 'use strict';
 
-var myApp = angular.module('marcopolo', ['ngRoute']);
+var myApp = angular.module('marcopolo', ['ngRoute','ngResource','marcopolo.Controllers','marcopolo.Services']);
 
 myApp.config(['$routeProvider', function ($routeProvider) {
       $routeProvider
       .when('/', {
-    	  templateUrl: 'resources/accueil.html'      	  
+    	  templateUrl: 'views/accueil.html'      	  
       })
       .when('/persons', {
-    	  templateUrl: 'resources/persons.html',
+    	  templateUrl: 'views/persons.html',
     	  controller: 'personCtrl'
       })
       .otherwise({
     	  redirectTo: '/'
       });
   }]);
-
-myApp.controller('personCtrl', function ($scope, $http) {
-	$http.get('http://localhost:8080/persons').
-		success(function(data){
-			$scope.listePersonnes = data;
-			$scope.unePersonne = data[0];
-		});	
-	});
 	
 })();
 
-
-
-
-
-/*
- $scope.listePersonnes = [
-	    {lastName: 'TOTO',firstName: 'Paul'},
-	    {lastName: 'LULU',firstName: 'Isa'},
-	    {lastName: 'BRIBRI',firstName: 'Carole'}
-	  ];
-	  $scope.unePersonne = {lastName:'Kiki'};
-	  */
