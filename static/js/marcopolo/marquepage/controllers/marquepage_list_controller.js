9177fe0a8fb8b062dc128c388fede60f9975b8eb
Cecile <cecile.cazaux@u-psud.fr>
2016-01-23 16:14:57 +0100

    mise à jour avec fichier controllers sur "marquages"

--- static/js/marcopolo/marquepage/controllers/marquepage_list_controller.js
+++ static/js/marcopolo/marquepage/controllers/marquepage_list_controller.js
@@ -1,30 +1,18 @@
 (function () {
-    'use strict';
+	'use strict';
 
-<<<<<<< HEAD
-    var myModuleController = angular.module('marcopolo.marquepageListCtrl',['ngResource','marcopolo.marquepageListService']);
-    
-    myModuleController.controller('marquepageListCtrl',['$scope','MarquepageListService','$routeParams','$location', function($scope,MarquepageListService,$routeParams,$location){
-      
-      $scope.listMqp = MarquepageListService.query();  
-      
-     /* var test=$routeParams.id_marquepage
+	var myModuleController = angular.module('marcopolo.marquepageListCtrl',['ngResource','marcopolo.marquepageListService']);
+
+	myModuleController.controller('marquepageListCtrl',['$scope','MarquepageListService','$routeParams','$location', function($scope,MarquepageListService,$routeParams,$location){
+
+		$scope.listMqp = MarquepageListService.query();  
+
+		/* var test=$routeParams.id_marquepage
       $scope.data = MarquepageListService.get({id:test});
       $scope.unMqp = $scope.data;*/
-	
-	$scope.paramId = MarquepageListService.get();
-	$scope.unMqp = $scope.paramId;	
-=======
-    angular
-        .module('marcopolo')
-        .controller('marquepageListCtrl', marquepageListCtrl);
 
-    marquepageListCtrl.$inject = ['$scope', '$resource', '$location', 'Marquepage'];
-    function marquepageListCtrl($scope, $resource, $location, Marquepage) {
+		$scope.paramId = MarquepageListService.get();
+		$scope.unMqp = $scope.paramId;	
 
-    } // function
->>>>>>> e510b19950ec52613773c8ae1fed557127bd0feb
-
-	      
-    }]);
+	}]);
 })();
