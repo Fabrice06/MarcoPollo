a118ecd598959e1b522778547ea01aafa5b5fbcd
Plats <fredericplats@gmail.com>
2016-02-07 19:19:07 +0100

    nettoyage de code

--- static/js/marcopolo/marquepage/controllers/marquepage_new_controller.js
+++ static/js/marcopolo/marquepage/controllers/marquepage_new_controller.js
@@ -3,22 +3,39 @@
 
     angular
         .module('marcopolo')
-        .controller('marquepageNewCtrl', marquepageNewCtrl);
+        .controller('marquepageNewCtrl',
+                    marquepageNewCtrl);
 
-    marquepageNewCtrl.$inject = ['$scope', '$resource', '$location', 'MarquepageNew','$routeParams'];
-    function marquepageNewCtrl($scope, $resource, $location, MarquepageNew, $routeParams) {
+    marquepageNewCtrl.$inject = ['$scope',
+                                 '$resource',
+                                 '$location',
+                                 'MarquepageNew',
+                                 '$routeParams'];
+    function marquepageNewCtrl($scope,
+                                $resource,
+                                $location,
+                                MarquepageNew,
+                                $routeParams) {
         
-       /* var url = '';
-        var nameMqp = '';*/
+        // recuperation id de l'utilisateur
         $scope.personId = $routeParams.personId;
         console.log("récup idPerson "+$scope.personId);
-        //$scope.mqpNewModel = {url: url, nameMqp: nameMqp};
+        // definition du Mqp
+        // $scope.mqpNewModel = {url: url, nameMqp: nameMqp};
         
+        // validation de la creation du nouveau Mqp :
         $scope.validate = function(mqpNewModel) {
 
-        	console.log("id : "+ $scope.personId+" lien : "+mqpNewModel.url+" nom : "+mqpNewModel.nameMqp);
-
-        	MarquepageNew.save({ id1:$scope.personId},{lien:mqpNewModel.url, nom:mqpNewModel.nameMqp}, 
+        	console.log("id : "
+                        + $scope.personId+" lien : "
+                        +mqpNewModel.url+" nom : "
+                        +mqpNewModel.nameMqp);
+            
+            // Post du formulaire
+        	MarquepageNew.save({ id1:$scope.personId},
+                               {lien:mqpNewModel.url,
+                                nom:mqpNewModel.nameMqp
+                               }, 
         	//Ca se passe bien
         	function (success) {
         		//on récupère l'id du mqp créé
@@ -27,37 +44,47 @@
         		var idMqp=idMqpArray[4];
         		console.log(idMqp);
 
-        		//on redirige vers la page détail        		
-        		$location.path('/persons/'+$scope.personId+'/marquepages/'+idMqpArray[4]);
+        		// Redirection vers la page détail du Mqp      		
+        		$location.path('/persons/'
+                               +$scope.personId
+                               +'/marquepages/'
+                               +idMqpArray[4]);
         		console.log($location.path());
         	},
+                               
         	// Ca se passe mal
         	function (pData, headers) {
         		console.log("marquepageLogCtl Query Echec");
         	})
         };
         
+        // clic sur le bouton annuler - retour sur la liste des mqp
         $scope.cancel = function() {
             //console.log("Canceling" + $scope.mqpNewModel.nameMqp + $scope.mqpNewModel.url);
 
             var nUrlArray=$location.url().split('/');
             var id = nUrlArray[2];
-            $location.path('/persons/'+id+'/marquepages').replace();
+            $location.path('/persons/'
+                           +id
+                           +'/marquepages').replace();
         };                
-        		
+        
 		 // clic sur le bouton déconnexion
         $scope.onProfil = function () {
 
             console.log("clic marquepageDetailCtrl déconnexion en cours");
-            // pour test uniquement
-            $location.path('/persons/2').replace();
+            nProfilLink = nUrlArray[1]
+                            +'/'
+                            +nUrlArray[2];
+            $location.path(nProfilLink).replace();
         };
 
-        // clic sur le bouton profil
+        /* clic sur le bouton profil - fonction retirée
         $scope.onExit = function () {
-
             $location.path('/').replace();
         };
+        */
+        
     } // function
 
 })();
