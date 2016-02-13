50af0d010764deb788c4249412fa551b45e6619b
CAZAUX Cecile <cecile.cazaux32@orange.fr>
2016-02-13 17:03:13 +0100

    affichage listMQP AVEC ou SANS tag

--- static/js/marcopolo/marquepage/services/marquepage_list_service.js
+++ static/js/marcopolo/marquepage/services/marquepage_list_service.js
@@ -7,18 +7,17 @@
     MarquepageList.$inject = ['$resource'];
     function MarquepageList ($resource) {
 
-        return $resource(
-            '/persons/:id/marquepages',
-            {},
-            { // return $resource('/users/:userId', {userId:'@id'}  ???
-            	'query': 	{method:'GET',		isArray: true},
-                'get': 	    {method:'GET',		isArray: false},
-                'save': 	{method:'POST', 	isArray: false},
-                'update': 	{method:'PUT', 		isArray: false},
-                'delete': 	{method:'DELETE', 	isArray: false}
-            }
-        );
+    	return $resource(
+    			'/persons/:id/marquepages',
+    			{},{ 
+    				'query': 	{method:'GET',		isArray: true},
+    				'get': 	    {method:'GET',		isArray: false},
+    				'save': 	{method:'POST', 	isArray: false},
+    				'update': 	{method:'PUT', 		isArray: false},
+    				'delete': 	{method:'DELETE', 	isArray: false}
+    			}
+    	);
 
-    } // function
+    } 
 
 })();
